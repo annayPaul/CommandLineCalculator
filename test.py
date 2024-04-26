9aceb8f35cc0441b89d18ef44c1cb55db6d089ff
Annay Paul <annay.paul@brainstation-23.com>
2024-04-26 13:34:18 +0600

    Two Test cases for running the substaction() function

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import unittest
-from calc import addition
+from calc import addition, substraction
 
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
         expected = addition(10, 20)
-        self.assertEqual(expected, 30) 
+        self.assertEqual(expected, 30)
+
+    def test_sub(self):
+        expected = substraction(20, 10)
+        self.assertEqual(expected, 10)
+        expected = substraction(-10, 10)
+        self.assertEqual(expected, -20)
+
