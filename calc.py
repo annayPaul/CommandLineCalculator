86679aa320e6782b9321c8f2ead6a06a3014d4a3
Ratul <ib.ratul008gmail.com>
2024-04-27 16:33:10 +0600

    Added the functionality to check if the numbers are even or odd

--- calc.py
+++ calc.py
@@ -1,12 +1,35 @@
 # File for Adding the features
 def addition(number1, number2):
-    return number1 + number2
+    inputInfo = f"{getEvenOrOddStatus(number1)}; + {getEvenOrOddStatus(number2)};"
+    equation = f"{number1} + {number2}"
+    result = number1 + number2
+    outputInfo = f"{getEvenOrOddStatus(result)}"
+    return f"{equation} = {result}\n{inputInfo} = {outputInfo}"
 
 def subtraction(number1, number2):
-    return number1 - number2
+    inputInfo = f"{getEvenOrOddStatus(number1)}; - {getEvenOrOddStatus(number2)};"
+    equation = f"{number1} - {number2}"
+    result = number1 + number2
+    outputInfo = f"{getEvenOrOddStatus(result)}"
+    return f"{equation} = {result}\n{inputInfo} = {outputInfo}"
 
 def multiplication(number1, number2):
-  return number1 * number2
+    inputInfo = f"{getEvenOrOddStatus(number1)}; * {getEvenOrOddStatus(number2)};"
+    equation = f"{number1} * {number2}"
+    result = number1 * number2
+    outputInfo = f"{getEvenOrOddStatus(result)}"
+    return f"{equation} = {result}\n{inputInfo} = {outputInfo}"
 
 def division(number1, number2):
-    return number1 // number2
+    inputInfo = f"{getEvenOrOddStatus(number1)}; / {getEvenOrOddStatus(number2)};"
+    equation = f"{number1} / {number2}"
+    result = number1 // number2
+    outputInfo = f"{getEvenOrOddStatus(result)}"
+    return f"{equation} = {result}\n{inputInfo} = {outputInfo}"
+
+def getEvenOrOddStatus(number):
+    if number % 2 == 0:
+        even_odd = "Even"
+    else:
+        even_odd = "Odd"
+    return f"{even_odd}"
