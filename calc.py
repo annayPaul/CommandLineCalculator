c613572562ee7809843c8314680841c0d3ceffdb
CNSAKIB <nazmuz.sakib@brainstation-23.com>
2024-04-28 23:18:02 +0600

    impliment PostiveorNegative function

--- calc.py
+++ calc.py
@@ -1,15 +1,31 @@
 # File for Adding the features
 def addition(number1, number2):
-    return number1 + number2
+    inputInfo=f"{getPositiveorNegativeStatus(number1)}; + {getPositiveorNegativeStatus(number2)};"
+    equation =f"{number1} + {number2}"
+    result = number1 + number2
+    outputInfo = f"{getPositiveorNegativeStatus(result)}"
+    return f"{equation}  = {result}\n{inputInfo} = {outputInfo}" 
 
 def subtraction(number1, number2):
-    return number1 - number2
+    inputInfo=f"{getPositiveorNegativeStatus(number1)}; + {getPositiveorNegativeStatus(number2)};"
+    equation =f"{number1} + {number2}"
+    result = number1 - number2
+    outputInfo = f"{getPositiveorNegativeStatus(result)}"
+    return f"{equation}  = {result}\n{inputInfo} = {outputInfo}" 
 
 def multiplication(number1, number2):
-  return number1 * number2
+    inputInfo=f"{getPositiveorNegativeStatus(number1)}; + {getPositiveorNegativeStatus(number2)};"
+    equation =f"{number1} + {number2}"
+    result = number1 * number2
+    outputInfo = f"{getPositiveorNegativeStatus(result)}"
+    return f"{equation}  = {result}\n{inputInfo} = {outputInfo}" 
 
 def division(number1, number2):
-    return number1 // number2
+    inputInfo=f"{getPositiveorNegativeStatus(number1)}; + {getPositiveorNegativeStatus(number2)};"
+    equation =f"{number1} + {number2}"
+    result = number1 // number2
+    outputInfo = f"{getPositiveorNegativeStatus(result)}"
+    return f"{equation}  = {result}\n{inputInfo} = {outputInfo}" 
 
 def getPositiveorNegativeStatus(number):
     if number<0:
